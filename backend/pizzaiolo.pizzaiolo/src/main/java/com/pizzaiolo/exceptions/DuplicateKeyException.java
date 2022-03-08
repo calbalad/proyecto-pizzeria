e432623f5c3fe5b5b27f62e96e9ebea47dd50abb
calbalad <christian.albaladejo-carrasco.capgemini.es>
2022-03-08 12:12:23 +0100

    security

---
+++ backend/pizzaiolo.pizzaiolo/src/main/java/com/pizzaiolo/exceptions/DuplicateKeyException.java
@@ -0,0 +1,31 @@
+package com.example.exceptions;
+
+public class DuplicateKeyException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public DuplicateKeyException() {
+		this("Duplicate Key");
+	}
+
+	public DuplicateKeyException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DuplicateKeyException(Throwable cause) {
+		super("Duplicate Key", cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DuplicateKeyException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DuplicateKeyException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
