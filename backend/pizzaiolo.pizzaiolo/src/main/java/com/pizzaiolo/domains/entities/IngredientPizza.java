18fa43133f6d02160bd0c39b2b8662c77c2296bb
PedroAix <pedroaixvinaroz@hotmail.com>
2022-03-13 19:58:16 +0100

    Modificación entities IngredientPizza e IngredientPizzaPK

--- backend/pizzaiolo.pizzaiolo/src/main/java/com/pizzaiolo/domains/entities/IngredientPizza.java
+++ backend/pizzaiolo.pizzaiolo/src/main/java/com/pizzaiolo/domains/entities/IngredientPizza.java
@@ -1,34 +1,48 @@
 package com.pizzaiolo.domains.entities;
 
 import java.io.Serializable;
-import javax.persistence.*;
+import java.util.Objects;
 
+import javax.persistence.*;
+import javax.validation.constraints.Positive;
+
+import com.pizzaiolo.domains.core.entities.EntityBase;
 
 /**
  * The persistent class for the IngredientPizza database table.
  * 
  */
 @Entity
-@NamedQuery(name="IngredientPizza.findAll", query="SELECT i FROM IngredientPizza i")
-public class IngredientPizza implements Serializable {
+@NamedQuery(name = "IngredientPizza.findAll", query = "SELECT i FROM IngredientPizza i")
+public class IngredientPizza extends EntityBase<IngredientPizza> implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@EmbeddedId
 	private IngredientPizzaPK id;
 
-	private int quantity;
-
-	//bi-directional many-to-one association to Ingredient
+	// bi-directional many-to-one association to Pizza
 	@ManyToOne
-	@JoinColumn(name="idIngredient")
-	private Ingredient ingredient;
-
-	//bi-directional many-to-one association to Pizza
-	@ManyToOne
-	@JoinColumn(name="idPizza")
+	@JoinColumn(name = "idPizza")
 	private Pizza pizza;
 
+	// bi-directional many-to-one association to Ingredient
+	@ManyToOne
+	@JoinColumn(name = "idIngredient")
+	private Ingredient ingredient;
+
+	@Column(name="quantity")
+	@Positive
+	private int quantity = 1;
+
 	public IngredientPizza() {
+	}
+	
+	public IngredientPizza(IngredientPizzaPK id, Pizza pizza, Ingredient ingredient, @Positive int quantity) {
+		super();
+		this.id = new IngredientPizzaPK(pizza.getIdPizza(), ingredient.getId());
+		this.pizza = pizza;
+		this.ingredient = ingredient;
+		this.quantity = quantity;
 	}
 
 	public IngredientPizzaPK getId() {
@@ -63,4 +77,20 @@ public class IngredientPizza implements Serializable {
 		this.pizza = pizza;
 	}
 
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		IngredientPizza other = (IngredientPizza) obj;
+		return Objects.equals(id, other.id);
+	}
 }
