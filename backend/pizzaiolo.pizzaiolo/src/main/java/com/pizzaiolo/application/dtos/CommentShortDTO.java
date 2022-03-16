b7d5577794a9bac56ef3eb13ece51fcc683b5c28
OscarSanDo <oscar.sanchez-domingo@capgemini.com>
2022-03-16 21:18:45 +0100

    comentarios swagger servicio comentarios

--- backend/pizzaiolo.pizzaiolo/src/main/java/com/pizzaiolo/application/dtos/CommentShortDTO.java
+++ backend/pizzaiolo.pizzaiolo/src/main/java/com/pizzaiolo/application/dtos/CommentShortDTO.java
@@ -3,15 +3,21 @@ package com.pizzaiolo.application.dtos;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.pizzaiolo.domains.entities.Comment;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Value;
 
 @Value
+@ApiModel(value = "Pizzas editable", description = "Version editable de las pizzas.")
 public class CommentShortDTO {
 	
+	@ApiModelProperty(value = "Identificador del comentario.")
 	@JsonProperty("idComentario")
 	private int idComment;
+	@ApiModelProperty(value = "Identificador del usuario que genera el comentario.")
 	@JsonProperty("idUsuario")
 	private String idUser;
+	@ApiModelProperty(value = "Texto del comentario.")
 	@JsonProperty("comentario")
 	private String text;
 
