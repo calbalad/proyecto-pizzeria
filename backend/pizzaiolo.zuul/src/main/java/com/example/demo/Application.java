c96a686a9da5588315b4eb6bb9c7f724c7259585
calbalad <christian.albaladejo-carrasco@capgemini.com>
2022-03-23 09:30:58 +0100

    init zuul project

--- backend/pizzaiolo.zuul/src/main/java/com/example/demo/Application.java
+++ backend/pizzaiolo.zuul/src/main/java/com/example/demo/Application.java
@@ -2,9 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableEurekaServer
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableZuulProxy
 @SpringBootApplication
 public class Application {
 
