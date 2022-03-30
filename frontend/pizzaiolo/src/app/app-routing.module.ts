78b7094212b532a5995e9ab6eadfb7c559386f47
OscarSanDo <oscar.sanchez-domingo@capgemini.com>
2022-03-30 17:37:40 +0200

    inicializacion utilidades

--- frontend/pizzaiolo/src/app/app-routing.module.ts
+++ frontend/pizzaiolo/src/app/app-routing.module.ts
@@ -3,32 +3,32 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
 
-  { path: '', pathMatch: 'full', component: PizzasComponent, data: { pageTitle: 'Pizzas' } },
-  { path: 'inicio', component: PizzasComponent, data: { pageTitle: 'Pizzas' } },
-  { path: 'registro', component: RegistroComponent, data: { pageTitle: 'Registro' } },
-  { path: 'login', component: LoginComponent, data: { pageTitle: 'Login' } },
-  { path: 'pizzas', component: PizzasComponent, data: { pageTitle: 'Pizzas' } },
-  { path: 'pizzas/:id', component: PizzasViewComponent, data: { pageTitle: 'Detalle Pizzas' } },
-  { path: 'carrito', component: CarritoComponent, data: { pageTitle: 'Carrito' } },
+  // { path: '', pathMatch: 'full', component: PizzasComponent, data: { pageTitle: 'Pizzas' } },
+  // { path: 'inicio', component: PizzasComponent, data: { pageTitle: 'Pizzas' } },
+  // { path: 'registro', component: RegistroComponent, data: { pageTitle: 'Registro' } },
+  // { path: 'login', component: LoginComponent, data: { pageTitle: 'Login' } },
+  // { path: 'pizzas', component: PizzasComponent, data: { pageTitle: 'Pizzas' } },
+  // { path: 'pizzas/:id', component: PizzasViewComponent, data: { pageTitle: 'Detalle Pizzas' } },
+  // { path: 'carrito', component: CarritoComponent, data: { pageTitle: 'Carrito' } },
 
-  { path: 'user', children: [
-    { path: '', component: UserComponent },
-    { path: 'edit', component: UserEditComponent },
-    { path: 'reset', component: UserResetComponent },
-  ]},
+  // { path: 'user', children: [
+  //   { path: '', component: UserComponent },
+  //   { path: 'edit', component: UserEditComponent },
+  //   { path: 'reset', component: UserResetComponent },
+  // ]},
 
-  { path: 'admin', children: [
-    { path: '', component: AdminComponent },
-    { path: 'ingredientes', component: AdminIngredientesComponent },
-    { path: 'pizzas', component: AdminPizzasComponent },
-    { path: ':roles', component: AdminRolesComponent },
-      ]},
+  // { path: 'admin', children: [
+  //   { path: '', component: AdminComponent },
+  //   { path: 'ingredientes', component: AdminIngredientesComponent },
+  //   { path: 'pizzas', component: AdminPizzasComponent },
+  //   { path: ':roles', component: AdminRolesComponent },
+  //     ]},
 
-  { path: 'tienda', children: [
+  // { path: 'tienda', children: [
 
-    { path: 'cocina', component: CocinaComponent },
-    { path: 'reparto', component: RepartoComponent },
-  ]},
+  //   { path: 'cocina', component: CocinaComponent },
+  //   { path: 'reparto', component: RepartoComponent },
+  // ]},
 ];
 
 @NgModule({
