155050298f71e2762a5f9bf95a56b49ce854be5d
OscarSanDo <oscar.sanchez-domingo@capgemini.com>
2022-03-30 12:57:49 +0200

    modulos genéricos Angular

--- frontend/pizzaiolo/src/app/main/main.module.ts
+++ frontend/pizzaiolo/src/app/main/main.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NotificationComponent } from './notification/notification.component';
+import { NotificationModalComponent } from './notification-modal/notification-modal.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NotificationComponent,
+    NotificationModalComponent,
+    HeaderComponent,
+    FooterComponent,
+    PageNotFoundComponent
+  ],
   imports: [
     CommonModule
   ]
