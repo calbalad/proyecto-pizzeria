287e86a412ad1d5f76bb73e5b65690f74226b929
PedroAix <pedroaixvinaroz@hotmail.com>
2022-04-01 14:50:58 +0200

    Añadido campos en User

--- frontend/pizzaiolo/src/app/user/user.module.ts
+++ frontend/pizzaiolo/src/app/user/user.module.ts
@@ -2,15 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserEditComponent, UserResetComponent } from './user.component';
 
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { CardModule } from 'primeng/card';
+import { PasswordModule } from 'primeng/password';
 
 
 @NgModule({
   declarations: [
-    UserEditComponent, UserResetComponent
+    UserEditComponent, UserResetComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule, InputTextModule, DropdownModule, ButtonModule, PasswordModule,
+    CardModule
   ],
-  exports: []
+  exports: [
+    CardModule, PasswordModule, ButtonModule, DropdownModule, InputTextModule,
+    UserEditComponent, UserResetComponent,
+  ]
 })
 export class UserModule { }
