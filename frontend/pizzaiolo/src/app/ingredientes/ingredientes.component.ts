ad22fd27647d1c29c60a8a856a8ae8d510153a54
OscarSanDo <oscar.sanchez-domingo@capgemini.com>
2022-04-02 12:45:56 +0200

    ingredientes api

--- frontend/pizzaiolo/src/app/ingredientes/ingredientes.component.ts
+++ frontend/pizzaiolo/src/app/ingredientes/ingredientes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IngredientesService } from '../services/ingredientes.service';
 
 @Component({
   selector: 'app-ingredientes',
@@ -8,74 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IngredientesComponent implements OnInit {
 
-  product = {
-    data: [
-      {
-        id: '1000',
-        code: 'f230fh0g3',
-        name: 'Bamboo Watch',
-        description: 'Product Description',
-        image: 'https://picsum.photos/200/200',
-        price: 65,
-        category: 'Pizzas',
-        quantity: 24,
-        inventoryStatus: 'INSTOCK',
-        rating: 5,
-      },
-      {
-        id: '1001',
-        code: 'nvklal433',
-        name: 'Black Watch',
-        description: 'Product Description',
-        image: 'https://picsum.photos/200/200',
-        price: 72,
-        category: 'Pizzas',
-        quantity: 61,
-        inventoryStatus: 'INSTOCK',
-        rating: 4,
-      },
-      {
-        id: '1002',
-        code: 'zz21cz3c1',
-        name: 'Blue Band',
-        description: 'Product Description',
-        image: 'https://picsum.photos/200/200',
-        price: 79,
-        category: 'Pizzas',
-        quantity: 2,
-        inventoryStatus: 'LOWSTOCK',
-        rating: 3,
-      },
-      {
-        id: '1002',
-        code: 'zz21cz3c1',
-        name: 'Blue Band',
-        description: 'Product Description',
-        image: 'https://picsum.photos/200/200',
-        price: 79,
-        category: 'Pizzas',
-        quantity: 2,
-        inventoryStatus: 'LOWSTOCK',
-        rating: 3,
-      },
-      {
-        id: '1002',
-        code: 'zz21cz3c1',
-        name: 'Blue Band',
-        description: 'Product Description',
-        image: 'https://picsum.photos/200/200',
-        price: 79,
-        category: 'Pizzas',
-        quantity: 2,
-        inventoryStatus: 'LOWSTOCK',
-        rating: 3,
-      }
-    ]
-  }
-
-  constructor() { }
+  ingredientes: any = [];
+  constructor(public restApi: IngredientesService) { }
 
   ngOnInit(): void {
+    this.restApi.getIngredientes().subscribe(
+        ( data: {} ) => {
+          this.ingredientes = data
+          console.log(this.ingredientes)
+        }
+      );
   }
+
 
 }
