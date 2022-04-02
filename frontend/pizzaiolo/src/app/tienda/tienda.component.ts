6c8e7c18767a7225804bb8442ca0fe8a38de76bb
PedroAix <pedroaixvinaroz@hotmail.com>
2022-04-02 17:56:18 +0200

    Creación de TiendaComponent

---
+++ frontend/pizzaiolo/src/app/tienda/tienda.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tienda',
+  templateUrl: './tmpl-cocina.component.html',
+  styleUrls: ['./tienda.component.scss']
+})
+export class CocinaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
+@Component({
+  selector: 'app-tienda',
+  templateUrl: './tmpl-reparto.component.html',
+  styleUrls: ['./tienda.component.scss']
+})
+export class RepartoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
